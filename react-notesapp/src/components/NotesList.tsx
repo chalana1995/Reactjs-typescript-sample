7da5df7bf90b81b5ab044fb8635a0b9f3456630c
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-11-12 23:36:33 +0530

    add componst and model

---
+++ react-notesapp/src/components/NotesList.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface INotesListProps {}
+
+const NotesList: React.FunctionComponent<INotesListProps> = (props) => {
+  return (
+    <>
+      <h2>Notes</h2>
+    </>
+  );
+};
+
+export default NotesList;
